3572d38aad4dad00e27b5199a443b5f56525f0eb
Andrew Chen <seak04@gmail.com>
2017-11-26 10:20:47 +0800

    test for DB bundle useage.

--- src/cq/bundle/console/Activator.java
+++ src/cq/bundle/console/Activator.java
@@ -6,11 +6,21 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Dictionary;
 import java.util.Hashtable;
+
+import javax.sql.DataSource;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
@@ -34,6 +44,7 @@ import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.osgi.framework.console.CommandInterpreter;
 import org.eclipse.osgi.framework.console.CommandProvider;
 import org.json.JSONArray;
@@ -46,9 +57,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.meowlomo.ci.ems.bundle.interfaces.BaseBundleActivator;
+import com.meowlomo.ci.ems.bundle.interfaces.IDataSource;
 import com.meowlomo.ci.ems.bundle.interfaces.IHttpUtil;
 import com.meowlomo.ci.ems.bundle.interfaces.IWebDriver;
 import com.meowlomo.ci.ems.bundle.interfaces.IHttpUtil.MethodType;
+import com.meowlomo.ci.ems.bundle.utils.StringUtil;
 
 import cq.bundle.console.FileJSONConvertor;
 
@@ -248,6 +261,150 @@ public class Activator extends BaseBundleActivator  implements CommandProvider  
 		 else{
 			 innerProcess(serviceRef, "F:\\_testcase\\repo1.xml", "F:\\_testcase\\testcase1.xlsm");
 		 }
+	}
+	
+	private void runDB(){
+		IDataSource ds = null;
+		logger.info("this is my host.");
+		try {
+			ds = getServiceObject(IDataSource.class);
+			EventAdmin eventAdmin = getEventAdmin();
+			if (null != eventAdmin) {
+				URL fileURL = FileLocator.toFileURL(getContext().getBundle().getEntry("dbconfig.xml"));
+				logger.info(getContext().getBundle().getEntry("dbconfig.xml").getFile());
+				
+				Dictionary<String, Object> msg = new Hashtable<String, Object>();
+				URL url2 = getContext().getBundle().getEntry("dbconfig.xml");
+				String urlPath = getContext().getBundle().getResource("/").getPath();
+//				msg.put("path", "C:\\workspace\\com.meowlomo.ci.beavor.bundle.db\\dbconfig.xml");
+				
+//				URI uri = new URL(StringUtil.encode(fileURL.toString(), "UTF-8")).toURI();
+				msg.put("path", "/dbconfig.xml");
+				Event reportGeneratedEvent = new Event("com/meowlomo/bundle/db/init", msg);
+				eventAdmin.postEvent(reportGeneratedEvent);
+				return;
+			}
+			
+			if (!ds.inited()){
+//				URL url1 = Activator.class.getResource("META-INF/MANIFEST.MF");
+//				URL url2 = Activator.class.getResource("dbconfig.xml");
+//				File dataFile = getContext().getBundle().getDataFile("utils.ssa");
+//				String dataFileStr = dataFile.getAbsolutePath();
+//				
+//				String url1 = getContext().getBundle().getEntry("META-INF/MANIFEST.MF").toURI().getPath();
+//				URL url2 = getContext().getBundle().getEntry("dbconfig.xml");
+//				String urlPath = getContext().getBundle().getResource("/").getPath();
+//				
+//				logger.info(Thread.currentThread().getContextClassLoader().getResource("").getPath());
+//				logger.info(url1.toString());
+//				logger.info(url2.toURI().toString());
+//				logger.info(url2.toString());
+//				
+////				File tttt = new File(url2.toURI());
+//				File ttwf2 = new File("C:/Program Files/eclipse/../../workspace/com.meowlomo.ci.beavor.bundle.usedb/dbconfig.xml");
+//				
+//				URL fileURL = FileLocator.toFileURL(getContext().getBundle().getEntry("dbconfig.xml"));
+//				logger.info(fileURL.toString());
+//				
+////				File ttwf2232 = new File(encode(fileURL.toString(), "UTF-8"));
+//				InputStream is = new URL(encode(fileURL.toString(), "UTF-8")).openStream();  
+//				printFileContent(is);
+//				
+//				File tmpFile = new File(fileURL.toURI());
+//				logger.info(fileURL.toURI().getPath().toString());
+//				logger.info(FileLocator.toFileURL(fileURL).getPath().toString());
+//				URI uri = FileLocator.toFileURL(fileURL).toURI();
+//				logger.info(uri.toString());
+
+				URL fileURL = FileLocator.toFileURL(getContext().getBundle().getEntry("dbconfig.xml"));
+				logger.info(getContext().getBundle().getEntry("dbconfig.xml").getFile());
+				logger.info(fileURL.toString());
+//				InputStream is = new URL(StringUtil.encode(fileURL.toString(), "UTF-8")).openStream();
+//				URI uri = new URL(StringUtil.encode(fileURL.toString(), "UTF-8")).toURI();
+//				ds.init(uri);
+//				ds.init("dbconfig.xml");
+//				ds.init("C:/workspace/com.meowlomo.ci.beavor.bundle.usedb/dbconfig.xml");
+//				ds.init("C:\\workspace\\com.meowlomo.ci.beavor.bundle.db\\dbconfig.xml");
+				ds.init("D:\\workspace\\eclipse\\cq.bundle.console\\dbconfig.xml");
+			}
+			if (ds.inited()){
+				DataSource dss = ds.getDataSource("oracleAoTain");
+				logger.info("get oracle ok.{}", dss.toString());
+			}
+		} catch (InstantiationException | IllegalAccessException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		logger.info(ds.toString());
+	}
+	
+	public void _runDB(CommandInterpreter ci){
+		runDB();
+	}
+	
+	
+	//demonstrate how to use the db bundle
+	public void _useDB(CommandInterpreter ci){
+		IDataSource ds = null;
+		logger.info("this is my host.");
+		Connection con = null;
+		try {
+			String dbName = ci.nextArgument();
+			ds = getServiceObject(IDataSource.class);
+			
+			String sql = "";
+			if (0 == dbName.compareToIgnoreCase("mysql1"))//"tencent_vps_mysql";
+				sql = "select count(1) as rows from bundle.logging_event;";
+			else if (0 == dbName.compareToIgnoreCase("postgresql1"))
+				sql = "select count(1) as totalemployee, sum(salary) as rows from cqtest;";
+//			else if (0 == dbName.compareToIgnoreCase("oracle1"))
+//				sql = "SELECT COUNT(ARCHITECTURE) AS rows FROM TEM_WORKERS";
+			else if (0 == dbName.compareToIgnoreCase("sqlserver1"))
+				sql = "select count(distinct(LastName)) as rows from testdb.dbo.Persons;";
+			else if (0 == dbName.compareToIgnoreCase("oracleAoTain"))
+//				sql = "insert into DAMS_AT_TTSS (name,address,code) values('andrew', '深圳', 1234)";
+				sql = "insert all into PUSH_PLATFORM.DOWN_ICP_HCJG_JRHC (WZID,WZBAXH,YM,JXIP,BBISP,HCJG,HCSJ,JRID,ISPID,REMARK,DEAL_FLAG,STATUS,DEAL_TIME,DEAL_REMARK,SHENGID,SHIID,DEAL_USERID) values (2001,'meowlomo_JRHC8','www.meowlomo.com',3232260090,1060,1,'2017-10-26 14:30:30',1008,1060,'meowlomoTest',1,3,to_date('26-11月-17','DD-MON-RR'),'批量未处理',440000,445200,892) into PUSH_PLATFORM.DOWN_ICP_HCJG_JRHC (WZID,WZBAXH,YM,JXIP,BBISP,HCJG,HCSJ,JRID,ISPID,REMARK,DEAL_FLAG,STATUS,DEAL_TIME,DEAL_REMARK,SHENGID,SHIID,DEAL_USERID) values (2002,'meowlomo_JRHC8','www.meowlomo.com   ',3232260090,1060,1,'2017-10-26 14:30:30',1008,1060,'meowlomoTest',1,3,to_date('26-11月-17','DD-MON-RR'),'批量未处理',440000,445200,892) into PUSH_PLATFORM.DOWN_ICP_HCJG_JRHC (WZID,WZBAXH,YM,JXIP,BBISP,HCJG,HCSJ,JRID,ISPID,REMARK,DEAL_FLAG,STATUS,DEAL_TIME,DEAL_REMARK,SHENGID,SHIID,DEAL_USERID) values (2003,'meowlomo_JRHC8','www.meowlomo.com   ',3232260090,1060,1,'2017-10-26 14:30:30',1008,1060,'meowlomoTest',2,3,to_date('26-11月-17','DD-MON-RR'),'批量未处理',440000,445200,892) into PUSH_PLATFORM.DOWN_ICP_HCJG_JRHC (WZID,WZBAXH,YM,JXIP,BBISP,HCJG,HCSJ,JRID,ISPID,REMARK,DEAL_FLAG,STATUS,DEAL_TIME,DEAL_REMARK,SHENGID,SHIID,DEAL_USERID) values (2003,'meowlomo_JRHC8','www.meowlomo.com   ',3232260090,1060,1,'2017-10-26 14:30:30',1008,1060,'meowlomoTest',2,3,to_date('26-11月-17','DD-MON-RR'),'批量未处理',440000,445200,892) select 1 from dual";
+			else if (0 == dbName.compareToIgnoreCase("oracle1"))
+				sql = String.format("Insert into AT_TEST (name,address,code) values ('andrew', '深圳', %d)", System.currentTimeMillis());
+			if (sql.isEmpty()){
+				logger.info("db name doesn`t match.");
+				return;
+			}
+			
+			DataSource dsService = ds.getDataSource(dbName);
+			con = dsService.getConnection();
+			Statement st = con.createStatement();
+			
+			if (sql.toLowerCase().contains("insert")){
+				System.out.println(st.executeUpdate(sql));
+				
+			}else{
+				 ResultSet rs = st.executeQuery(sql);
+				 ResultSetMetaData rsmd = rs.getMetaData();
+					
+				int columnCount = rsmd.getColumnCount();
+				boolean bInserted = rs.rowInserted();
+				rs.last();
+				int rowCount = rs.getRow();
+				for(int i = 1; i <= columnCount; ++i){
+					logger.info(rsmd.getColumnName(i));
+				}
+				if (rs.next()){
+//					int rowcount = rs.getInt("arow");
+					int rowcount = rs.getInt(1);
+					logger.info(String.format("The row count of target is %d", rowcount));
+				}
+			}
+		} catch (InstantiationException | IllegalAccessException | SQLException e){
+			logger.error("using db error:", e);
+		} finally{
+			if (null != con)
+				try {
+					con.close();
+				} catch (SQLException e) {
+					logger.error("close sql connection error:", e);
+				}
+		}
 	}
 	
 	public EventAdmin getEventAdmin(){
